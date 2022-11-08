54b7fa5170d26e1468ef3eb45fb59eee7865cf73
omar0425 <63131145+omar0425@users.noreply.github.com>
2022-11-08 10:54:31 -0500

    Update dojo_controller.rb

--- app/controllers/dojo_controller.rb
+++ app/controllers/dojo_controller.rb
@@ -1,5 +1,18 @@
 class DojoController < ApplicationController
+  
+  
+  
+  get '/dojos' do
+    dojos = Dojo.all
+    dojos.to_json
+  end
 
+  post '/dojos' do
+    dojo = Dojo.create(
+      name: params[:name]
+    )
+    dojo.to_json
+  end
 
 
 
